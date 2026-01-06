624d19c14c68ce858e243c5d47d58263bdb28a6f
Tejas Sahebrao Gawali <156706283+TejasSahebraoGawali@users.noreply.github.com>
2026-01-06 15:21:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-//your JS code here. If required.
+document.addEventListener("DOMContentLoaded", function (event) {
+
+    const codeInputs = document.querySelectorAll(".code-container .code");
+
+    codeInputs[0].focus();
+
+    for (let input of codeInputs) {
+        input.addEventListener("keyup", function (event) {
+            let target = event.target;
+            if (target.value) {
+                target.nextElementSibling?.focus();
+            } else if (!target.value && event.key === "Backspace") {
+                target.previousElementSibling?.focus();
+            }
+        })
+    }
+
+
+})
